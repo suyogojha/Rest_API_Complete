b0c869819dff9412ea2dcb887437fadec172d591
suyog ojha <ojhasuyog2020@gmail.com>
2023-02-11 11:31:59 +0545

    @CustomQueryset

--- backend/cfehome/urls.py
+++ backend/cfehome/urls.py
@@ -1,10 +1,12 @@
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/products/', include('products.urls'))
+    path('api/products/', include('products.urls')),
+    path('api/v2/', include('cfehome.routers'))
 ]
 
 # localhost:8000/api/
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,19 +1,57 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Product
-
+from . import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+            view_name='product-detail',
+            lookup_field='pk'
+    )
+    title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
+    # email = serializers.CharField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
+            # 'user',
+            'url',
+            'edit_url',
+            'pk',
             'title',
+            # 'name',
             'content',
             'price',
             'sale_price',
             'my_discount',
         ]
+    
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
+    
+    # def create(self, validated_data):
+    #     # return Product.obejcts.create(**validated_data)
+    #     # email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     # print(email, obj)
+    #     return obj
+    
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop('email')
+    #     return super().update(instance, validated_data)
+    
+    def get_edit_url(self, obj):
+        request = self.context.get('request') # self.request
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request) 
     
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
